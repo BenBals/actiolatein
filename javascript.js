1fdf2bd769da3d9afff920dc9201b60c7ab173a3
Ben <benjustusbals@gmail.com>
2014-04-10 19:38:28 +0200

    quick fix

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 function stammformenGoTo () {
-	window.location.href = "/stammformen/index.html";
+	window.location.href = "stammformen/index.html";
 }
 function vokabelnGoTo () {
-	window.location.href = "/vokabeln/index.html";
+	window.location.href = "vokabeln/index.html";
 }
 function dlapp () {
 	window.open('https://s3-eu-west-1.amazonaws.com/stammformenapp/Actiolatein.apk', '_blank');

777803d4e61aa2b02b8e3848b5d6d317d9bb0aad
Ben <benjustusbals@gmail.com>
2014-04-13 14:38:03 +0200

    conjugation huge update and more
    tags, e-konj, konj-js, minor fixes

--- konjugation/javascript.js
+++ konjugation/javascript.js
@@ -1,97 +1,114 @@
 window.score = 0;
 window.upTo6 = 0;
 
-function aKonj () {
+function aKonjPraAk () {
 	window.konj = "a";
+	window.akPa = "ak";
+	window.zeit = "pra";
+	$(".aw").css("display","inline");
 	$("#question0").css("display","none");
-	$("#aKonj").css("display","block");
+	$("#aKonj_pra_ak").css("display","block");
+}
+function eKonjPraAk () {
+	window.konj = "e";
+	window.akPa = "ak";
+	window.zeit = "pra";
+	$(".aw").css("display","inline");
+	$("#question0").css("display","none");
+	$("#eKonj_pra_ak").css("display","block");
 }
 
+function homeKonj () {
+	window.location.href = "../index.html";
+}
 
-//1 A
+function auswahl () {
+	window.location.reload();
+}
+
 function checkIt () {
-	var input1s = $("#input1s_"+window.konj).val().toLowerCase();
-	var input2s = $("#input2s_"+window.konj).val().toLowerCase();
-	var input3s = $("#input3s_"+window.konj).val().toLowerCase();
-	var input1p = $("#input1p_"+window.konj).val().toLowerCase();
-	var input2p = $("#input2p_"+window.konj).val().toLowerCase();
-	var input3p = $("#input3p_"+window.konj).val().toLowerCase();
-	var answer1s = $("#answer1s_"+window.konj).text().toLowerCase();
-	var answer2s = $("#answer2s_"+window.konj).text().toLowerCase();
-	var answer3s = $("#answer3s_"+window.konj).text().toLowerCase();
-	var answer1p = $("#answer1p_"+window.konj).text().toLowerCase();
-	var answer2p = $("#answer2p_"+window.konj).text().toLowerCase();
-	var answer3p = $("#answer3p_"+window.konj).text().toLowerCase();
+	var input1s = $("#input1s_"+window.zeit+"_"+window.akPa+"_"+window.konj).val().toLowerCase();
+	var input2s = $("#input2s_"+window.zeit+"_"+window.akPa+"_"+window.konj).val().toLowerCase();
+	var input3s = $("#input3s_"+window.zeit+"_"+window.akPa+"_"+window.konj).val().toLowerCase();
+	var input1p = $("#input1p_"+window.zeit+"_"+window.akPa+"_"+window.konj).val().toLowerCase();
+	var input2p = $("#input2p_"+window.zeit+"_"+window.akPa+"_"+window.konj).val().toLowerCase();
+	var input3p = $("#input3p_"+window.zeit+"_"+window.akPa+"_"+window.konj).val().toLowerCase();
+	var answer1s = $("#answer1s_"+window.zeit+"_"+window.akPa+"_"+window.konj).text().toLowerCase();
+	var answer2s = $("#answer2s_"+window.zeit+"_"+window.akPa+"_"+window.konj).text().toLowerCase();
+	var answer3s = $("#answer3s_"+window.zeit+"_"+window.akPa+"_"+window.konj).text().toLowerCase();
+	var answer1p = $("#answer1p_"+window.zeit+"_"+window.akPa+"_"+window.konj).text().toLowerCase();
+	var answer2p = $("#answer2p_"+window.zeit+"_"+window.akPa+"_"+window.konj).text().toLowerCase();
+	var answer3p = $("#answer3p_"+window.zeit+"_"+window.akPa+"_"+window.konj).text().toLowerCase();
 
 	if (input1s == answer1s) {
 		window.score++;
 		window.upTo6++;
-		$ ("#input1s_"+window.konj).css("border-color","#69BB9C");
+		$("#input1s_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("border-color","#69BB9C");
 	}
 	else {
-		$ ("#input1s_"+window.konj).css("border-color","#CB4C39");
-		$ ("#answer1s_"+window.konj).css("display","inline");
+		$("#input1s_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("border-color","#CB4C39");
+		$("#answer1s_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("display","inline");
 	}
 	
 	if (input2s == answer2s) {
 		window.score++;
 		window.upTo6++;
-		$ ("#input2s_"+window.konj).css("border-color","#69BB9C");
+		$("#input2s_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("border-color","#69BB9C");
 	}
 	else {
-		$ ("#input2s_"+window.konj).css("border-color","#CB4C39");
-		$ ("#answer2s_"+window.konj).css("display","inline");
+		$("#input2s_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("border-color","#CB4C39");
+		$("#answer2s_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("display","inline");
 	}
 	
 	if (input3s == answer3s) {
 		window.score++;
 		window.upTo6++;
-		$ ("#input3s_"+window.konj).css("border-color","#69BB9C");
+		$ ("#input3s_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("border-color","#69BB9C");
 	}
 	else {
-		$ ("#input3s_"+window.konj).css("border-color","#CB4C39");
-		$ ("#answer3s_"+window.konj).css("display","inline");
+		$("#input3s_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("border-color","#CB4C39");
+		$("#answer3s_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("display","inline");
 	}
 	
 	if (input1p == answer1p) {
 		window.score++;
 		window.upTo6++;
-		$ ("#input1p_"+window.konj).css("border-color","#69BB9C");
+		$("#input1p_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("border-color","#69BB9C");
 	}
 	else {
-		$ ("#input1p_"+window.konj).css("border-color","#CB4C39");
-		$ ("#answer1p_"+window.konj).css("display","inline");
+		$("#input1p_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("border-color","#CB4C39");
+		$("#answer1p_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("display","inline");
 	}
 	
 	if (input2p == answer2p) {
 		window.score++;
 		window.upTo6++;
-		$ ("#input2p_"+window.konj).css("border-color","#69BB9C");
+		$("#input2p_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("border-color","#69BB9C");
 	}
 	else {
-		$ ("#input2p_"+window.konj).css("border-color","#CB4C39");
-		$ ("#answer2p_"+window.konj).css("display","inline");
+		$("#input2p_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("border-color","#CB4C39");
+		$("#answer2p_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("display","inline");
 	}
 	
 	if (input3p == answer3p) {
 		window.score++;
 		window.upTo6++;
-		$ ("#input3p_"+window.konj).css("border-color","#69BB9C");
+		$("#input3p_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("border-color","#69BB9C");
 	}
 	else {
-		$ ("#input3p_"+window.konj).css("border-color","#CB4C39");
-		$ ("#answer3p_"+window.konj).css("display","inline");
+		$("#input3p_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("border-color","#CB4C39");
+		$("#answer3p_"+window.zeit+"_"+window.akPa+"_"+window.konj).css("display","inline");
 	}
 	
 	if (window.upTo6 == 6) {
-		$ (".allRight").css("display","block");
+		$(".allRight").css("display","block");
 	}
 	if (window.upTo6 === 0) {
-		$ (".nothingRight").css("display","block");
+		$(".nothingRight").css("display","block");
 	}
 	else {
 		if (window.upTo6 != 6) {
-			$ (".notAllRight").css("display","block");
+			$(".notAllRight").css("display","block");
 		}
 	}
 	window.upTo6 = 0;

51e00367ca2b0e517326658bd2fd4fc62a87f518
Ben <benjustusbals@gmail.com>
2014-04-26 16:42:21 +0200

    added a konj

--- deklination/javascript.js
+++ deklination/javascript.js
@@ -1,12 +1,16 @@
 window.klasse = "r";
 window.allClasses = ["nom","gen","dat","akk","abl","vok"];
 window.allClassesUp = ["Nom","Gen","Dat","Akk","Abl","Vok"];
+window.upTo12 = 0;
 
 function showQs (klasseZahl) {
+
 	if (klasseZahl === 1) {window.klasse = "o";}
 
 	$("#question0").css("display","none");
 	$("#questionScreen").css("display","block");
+	$(".back").css("display","inline");
+	$("#questionHere").html($("#q_"+window.klasse).html());
 
 	//A @ the end = Answer || I @ the end = Input
 
@@ -19,19 +23,45 @@ function showQs (klasseZahl) {
 	
 	//setting the html() of the answers that are shown
 	for (var i = 0; i<6; i++) {
-
+		$("#answer"+window.allClassesUp[i]+"Sg").html(window[window.allClasses[i]+"SgA"]);
 	}
-	$("#answerNomSg").html(window.nomSgA);
-	$("#answerGenSg").html(window.genSgA);
-	$("#answerDatSg").html(window.datSgA);
-	$("#answerAkkSg").html(window.akkSgA);
-	$("#answerAblSg").html(window.ablSgA);
-	$("#answerVokSg").html(window.vokSgA);
-	$("#answerNomPl").html(window.nomPlA);
-	$("#answerGenPl").html(window.genPlA);
-	$("#answerDatPl").html(window.datPlA);
-	$("#answerAkkPl").html(window.akkPlA);
-	$("#answerAblPl").html(window.ablPlA);
-	$("#answerVokPl").html(window.vokPlA);
+	for (var i = 0; i<6; i++) {
+		$("#answer"+window.allClassesUp[i]+"Pl").html(window[window.allClasses[i]+"PlA"]);
+	}
 }
 
+function back () {
+	$("#question0").css("display","block");
+	$("#questionScreen").css("display","none");
+}
+
+function checkDek () {
+	//Declareing the input vars from fields
+	for (var i = 0; i<6; i++) {
+		window[window.allClasses[i]+"SgI"] = $("#input"+window.allClassesUp[i]+"Sg").val().toLowerCase();
+	}
+	for (var i = 0; i<6; i++) {
+		window[window.allClasses[i]+"PlI"] = $("#input"+window.allClassesUp[i]+"Pl").val().toLowerCase();
+	}
+	for (var i = 0; i<6; i++) {
+		if (window[window.allClasses[i]+"SgI"] == window[window.allClasses[i]+"SgA"]) {
+			window.upTo12++;
+			$("#input"+window.allClassesUp[i]+"Sg").css("border-color","#69BB9C");
+		}
+		else {
+			$("#input"+window.allClassesUp[i]+"Sg").css("border-color","#CB4C39");
+			$("#answer"+window.allClassesUp[i]+"Sg").css("display","inline");
+		}
+	}
+	for (var i = 0; i<6; i++) {
+		if (window[window.allClasses[i]+"PlI"] == window[window.allClasses[i]+"PlA"]) {
+			window.upTo12++;
+			$("#input"+window.allClassesUp[i]+"Pl").css("border-color","#69BB9C");
+		}
+		else {
+			$("#input"+window.allClassesUp[i]+"Pl").css("border-color","#CB4C39");
+			$("#answer"+window.allClassesUp[i]+"Pl").css("display","inline");
+		}
+	}
+
+}
